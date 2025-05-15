7800c95adedda554f774e26ba80cefffe716a7c8
Gül Nihal Temel <diolasciarmi@gmail.com>
2025-05-15 13:09:30 +0300

    feat: demonstrate output methods using console.log, document.write, and alert

---
+++ output methods of JS/app.js
@@ -0,0 +1,23 @@
+//* Document Write
+    document.write("Hello World <br>");
+    document.write("Gülnihal <br>");
+    document.writeln(55);
+    document.write(5.5);
+    document.write("<br>");
+    document.writeln(5*12);
+    
+//* Console Log
+//   console.log("Hello World");
+//   console.log("Gülnihal Temel");
+//   console.log(3.53);
+// console.log(true);
+//  console.log({name:"Gülnihal", surname:"Temel"});
+//  let a=5;
+//  let b=12;
+//  console.log("Added value of two numbers : "+ (a+b));
+//  console.clear();
+  
+
+//*Using alert box to display data
+   // alert("Hello"); //pop up message
+ //alert("Do not use the F5 button.");
