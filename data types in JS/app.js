4504cecab63a6bb86989e867bb357c5b83d2e01b
Gül Nihal Temel <diolasciarmi@gmail.com>
2025-05-19 23:05:27 +0300

    feat: include overview of JavaScript data types

---
+++ data types in JS/app.js
@@ -0,0 +1,71 @@
+// JavaScript  DataTypes
+/*
+
+String
+Number
+Function
+Boolean
+Undefined
+Null
+Object
+
+*/
+//! STRING
+//let name = "She is 20 years old. ";
+//console.log(name);
+//console.log(typeof name);
+//! NUMBER
+//let number1 = 10.7;
+//let number2 = 5.3;
+//console.log(number1 + number2);
+//console.log(typeof number1 );
+
+//let a="5";
+//let b = "2";
+//console.log(a +b);                //52
+
+//let stringVariable = "Your age : ";
+//let age = 20;
+//console.log(stringVariable + age);
+//! BOOLEANS
+//? Booleans can only have two values : true or false.
+//console.log(5>8);
+// let a = 3;
+// let b =12;
+//let total = a+b;
+//console.log(total==15);
+//! NULL
+
+/*let a = null;
+
+a =27;
+
+console.log(a);
+*/
+
+//!UNDEFINED
+
+//let a;
+//console.log(typeof a);
+
+//! OBJECT
+
+//let human = {
+//    name : "Gülnihal",
+//    surname:"Temel",
+//    age : 20
+//}
+//console.log(human);
+//console.log(typeof human);
+
+//let numbers = [1,2,3,4,5]; 
+//console.log(typeof numbers);
+
+
+//! FUNCTION 
+
+//let func = function(){
+//    console.log("Hello");
+//}
+//func();
+//console.log(typeof func);
