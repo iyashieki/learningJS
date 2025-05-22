0d68470c7d0e064cbb22f22fad526bbfb8085e57
Gül Nihal Temel <diolasciarmi@gmail.com>
2025-05-22 12:19:19 +0300

    feat: implement basic arithmetic operators in JavaScript

---
+++ arithmetic operators/app.js
@@ -0,0 +1,66 @@
+
+/*
+
++	Addition
+-	Subtraction
+*	Multiplication
+**	Exponentiation (ES2016)
+/	Division
+%	Modulus (Remainder)
+++	Increment
+-- Decrement
+
+*/
+//* ASSIGNMENT OPERATOR
+//let a = 5;
+//let name = "Gülnihal";
+ 
+//* ADDITION
+//console.log(5+7);
+
+//let grade1 = 100;
+//let grade2 = 70;
+
+//let number1 = "5";
+//let number2 = 2;
+
+//console.log(number1 + number2); //string + number     stringnumber
+
+//console.log( "Total Grade : " + (grade1 + grade2));
+//* SUBSTRACTION
+//console.log(grade1-grade2);
+
+//* MULTIPLICATION
+
+//let num1 = 5;
+//let num2 = 10;
+
+//console.log(num1*num2);
+
+//* DIVISION 
+
+//let grade1 = 100;
+//let grade2 = 70;
+
+//let finalGrade = (grade1 + grade2)/2;
+
+//console.log("End of year Grade : " + finalGrade);
+
+//* MODULUS 
+/* 10/3 -> 1
+ 12/5 -> 2 */
+
+//console.log(12%5);
+//console.log(10%3);
+
+//* INCREMENT and DECREMENT
+
+//let a = 10;
+//a++; //11
+//a--; //10
+//console.log(a);
+
+//* Exponentiation
+// 2^3=8
+
+console.log(5**3);
