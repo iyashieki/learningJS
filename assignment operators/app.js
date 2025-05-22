b67ade758af24ce1eb2e791b37f5f3313e49804e
Gül Nihal Temel <diolasciarmi@gmail.com>
2025-05-22 18:34:01 +0300

    feat: implement basic and compound assignment operators in JavaScript

---
+++ assignment operators/app.js
@@ -0,0 +1,58 @@
+/*          ASSIGNMENT OPERATORS
+
+OPERATOR        EXAMPLE            SAME AS
+=	            x = y	            x = y
++=	            x += y	            x = x + y
+-=	            x -= y	            x = x - y
+*=	            x *= y	            x = x * y
+/=	            x /= y	            x = x / y
+%=	            x %= y	            x = x % y
+**=	            x **= y	            x = x ** y
+
+*/
+
+//? Simple Assignment Operator (=) assigns a value to a variable.
+//let a = 5; 
+
+//console.log(5==5);  // console : True. This is a comparison operator. 
+
+
+// === ; equal value and equal type. IS a comparison operator.
+/*
+console.log(5===5); //console : True
+console.log(5==="5"); //console : False
+*/ 
+
+//? Addition Assignment Operator (+=) adds a value to a variable.
+//let number = 4;
+// number += 2;
+// console.log(number);
+
+//? Subtraction Assignment Operator (-=) subtracts a value from a variable.
+
+//let num = 10;
+//num -= 2;
+//console.log(num);
+
+//? Multiplication Assignment Operator (*=) multiplies a variable.
+
+//let number = 20;
+//number *= 3;
+//console.log(number);
+
+//? Division Assignment Operator (/=) divides a variable.
+
+//let number = 40;
+//number /= 10;
+//console.log(number); 
+
+//? Remainder Assignment Operator (%=) assigns a remainder to a variable.
+//let number = 80;
+//number %= 9;
+//console.log(number);
+
+//? Exponentiation Assignment Operator (**=) raises a variable to the power of the operand.
+let number = 5;
+number **= 3;
+console.log(number);
+
