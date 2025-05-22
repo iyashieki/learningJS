c4c879c2030c40a1083aa79d7f5c0d450f221c34
Gül Nihal Temel <diolasciarmi@gmail.com>
2025-05-22 22:24:49 +0300

    feat: implement comparison operators in JavaScript

---
+++ comparison operators/app.js
@@ -0,0 +1,30 @@
+/*  while x=5
+    Operator	            Description 	          Comparing	    Returns
+
+    ==	                 equal to	                x == 8	    false	
+                                                    x == 5	    true	
+                                                    x == "5"	true	
+    ===	        equal value and equal type	        x === 5	    true	
+                                                    x === "5"	false	
+    !=	                   not equal	            x != 8	    true	
+    !==	        not equal value or not equal type	x !== 5	    false	
+                                                    x !== "5"	true	
+                                                    x !== 8	    true	
+    >	                greater than	            x > 8	    false	
+    <	                  less than	                x < 8	    true	
+    >=	            greater than or equal to	    x >= 8	    false	
+    <=	                less than or equal to	     x <= 8	    true	
+
+*/
+
+//console.log(5!=3); //true
+
+//console.log(5!=5); //false
+
+//let age =20;
+//console.log(age!=23);
+
+//console.log(11>12); //false
+//console.log(12<12);// false
+//console.log(12<=12); //true
+//console.log(13>=12); //true
