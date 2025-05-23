f058a406de1a832fe0711965b1e3f0aebae6aa40
Gül Nihal Temel <diolasciarmi@gmail.com>
2025-05-23 17:58:08 +0300

    feat: implement JavaScript popup boxes.

---
+++ popup boxes /app.js
@@ -0,0 +1,28 @@
+/* JAVA SCRIPT POPUP BOXES
+
+    Alert Box: is often used if you want to make sure information comes through to the user.
+When an alert box pops up, the user will have to click "OK" to proceed.
+
+    Confirm Box: is often used if you want the user to verify or accept something.When a confirm box pops up, the user will have to click either "OK" or "Cancel" to proceed.
+If the user clicks "OK", the box returns true. If the user clicks "Cancel", the box returns false.
+
+    Prompt Box: A prompt box is often used if you want the user to input a value before entering a page.
+When a prompt box pops up, the user will have to click either "OK" or "Cancel" to proceed after entering an input value.
+If the user clicks "OK" the box returns the input value. If the user clicks "Cancel" the box returns null.
+
+
+*/
+//console.log(window);
+
+//alert("I am an alert box!");
+
+//let name = prompt("Enter your name: ");
+//let age = prompt("Enter your age: ");
+//console.log("Your Name: " + name);
+//console.log("Your age: "+ age);
+//onsole.log(typeof name);  //string
+//console.log(typeof age);    //string
+
+let resultant = confirm("Permanently delete the backup?");
+
+console.log(resultant);
