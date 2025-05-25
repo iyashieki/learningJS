244dd0bf43e181f6aff4a6c3f0a5377c08e7b7eb
Gül Nihal Temel <diolasciarmi@gmail.com>
2025-05-25 15:23:49 +0300

    feat: implement explicit type casting for consistent data handling

---
+++ type-conversion/app.js
@@ -0,0 +1,56 @@
+//strings, numbers, booleans, undefined, null, object, function.
+
+//? CONVERTING STRINGS TO NUMBERS
+//? Methods __ Number() ; parseFloat() ; parseInt() .
+//let a =5;
+//let b = Number("10");
+//let c = Number(b);
+// console.log(typeof c);
+//ConvolverNode.log(a +c);
+
+//parseInt()
+//console.log(window);
+
+//let a = 8;
+//let b =parseInt("12");
+//console.log(a +b);
+//console.log(typeof b);
+
+/*
+let c = parseFloat("12.5"); // string --> number(float)
+console.log(c);
+console.log(typeof c); 
+*/
+
+//? CONVERTING NUMBERS TO STRINGS
+//? .toString() ; String()
+
+
+/*
+let x = (55).toString();
+//let x= String(55);
+console.log(typeof x),
+console.log(x);
+*/
+
+/*
+let outcome = String(true);
+console.log( typeof outcome); // boolean --> string
+console.log(outcome);
+*/
+
+
+/*
+let a = Number("B");
+//Not a Number : NaN
+console.log(a);
+*/
+
+/*
+let numbers = String([1,2,3,4]);
+console.log(typeof numbers); //object --> String
+console.log( numbers);
+*/
+
+let numeral = Number([1,2,3,4]);
+console.log(numeral); //NaN
