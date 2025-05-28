f874cbec196f73352d0fb2f0c817f96fe09c37de
Gül Nihal Temel <diolasciarmi@gmail.com>
2025-05-29 02:06:56 +0300

    feat: add introductory array usage in JavaScript

---
+++ arrays/app.js
@@ -0,0 +1,13 @@
+//  ARRAYS
+
+// ArrayIndexOutOfBoundException
+// let numbers = [0,1,2,3,4,5,"Nihal",7,8,9,"Hava"];
+// numbers[numbers.length-1] = "Mehmet";
+// console.log(numbers[10]);
+
+// let names = ["Nihal","Kübra","Bilal","Yusuf"];
+// names[2] = "Yakup";
+// console.log(names);
+
+// let mixedArray = [1,"Nihal",5.7,true,null,undefined];
+// console.log(mixedArray[3]);
