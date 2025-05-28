524f528b8111f7e74b5f902b44804dc57c0fe351
Gül Nihal Temel <diolasciarmi@gmail.com>
2025-05-29 02:28:05 +0300

    feat: add introductory array usage with forEach loop in JavaScript

---
+++ foreach loop/app.js
@@ -0,0 +1,26 @@
+// For loop, while loop, do while loop, foreach loop
+// foreach is used with arrays
+
+// FOREACH LOOP EXAMPLE
+
+let names = ["Enes", "Yakup", "Bilal", "Kübra", "Aysenur", "Adem"];
+
+// Using while loop
+let meter = 0;
+while (meter < names.length) {
+    console.log(names[meter]);
+    meter++;
+}
+
+
+/*
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
+*/
+
+/*
+names.forEach(function(name) {
+    console.log(name);
+});
+*/
