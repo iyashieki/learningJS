e7d7488acdee06722eac948630065a1f75feeb3c
Gül Nihal Temel <diolasciarmi@gmail.com>
2025-06-01 00:17:03 +0300

    feat: add examples for common JavaScript string methods

---
+++ string methods/app.js
@@ -0,0 +1,89 @@
+//*STRING METHODS
+
+let course = "Modern Web Development Course" ;
+
+/*
+charAt()
+concat()
+indexof()
+lastindexof()
+toUpperCase()
+toLowerCase()
+trim()
+slice()
+subString()
+replace()
+split()
+valueOf()
+startsWith()
+endsWith()
+*/ 
+
+//? .charAt()
+/*
+let character = course.charAt(5);
+
+console.log(character);
+*/
+
+//? CONCAT METHOD
+/*
+let result = course.concat(" ", "date", " Enes");
+console.log(result);
+*/
+
+//? INDEXOF METHOD
+/*
+let index = course.indexOf("W");
+console.log(index);
+*/
+
+// let index = course.lastIndexOf("K");
+// console.log(index);
+
+//? TOUPPERCASE METHOD
+
+/*
+let result = course.toUpperCase();
+ console.log(course);
+ console.log(result);
+ */
+
+//? TOLOWERCASE METHOD
+// let result = course.toLowerCase();
+// console.log(result);
+
+//? TRIM METHOD
+// console.log(course.trim());
+
+//? SLICE METHOD
+// console.log(course);
+// console.log(course.slice(22, 28));
+
+//? SUBSTRING METHOD
+// console.log(course);
+// console.log(course.substring(0, 6));
+
+//? REPLACE METHOD
+// console.log(course);
+// console.log(course.replace("Modern", "Updated"));
+
+//? SPLIT METHOD
+// console.log(course);
+// let arr = course.split(" ");
+// console.log(arr);
+
+//? VALUEOF METHOD
+// console.log(course.valueOf());
+
+//? STARTSWITH METHOD
+// console.log(course);
+// console.log(course.startsWith("M"));
+// if (course.startsWith("M")) {
+//     console.log("Starts with the letter M.");
+// }
+
+//? ENDSWITH METHOD
+//console.log(course);
+// console.log(course.endsWith("Development Course"));
+
