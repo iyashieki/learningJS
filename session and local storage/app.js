f30fde86eca4aa9291fe5aa667fb0f2153cad600
Gül Nihal Temel <diolasciarmi@gmail.com>
2025-06-19 21:35:46 +0300

    feat: demonstrate usage of sessionStorage and localStorage with string and array data

---
+++ session and local storage/app.js
@@ -0,0 +1,64 @@
+// Session Storage
+
+// Add Values
+// sessionStorage.setItem("350", "Enes");
+// sessionStorage.setItem("216", "Yasin");
+// sessionStorage.setItem("154", "Bilal");
+// sessionStorage.setItem(552, 37);
+
+// Remove Value
+// sessionStorage.removeItem("154");
+
+
+// let value = sessionStorage.getItem("350");
+// if (value === null) {
+//     console.log("Value not found.");
+// } else {
+//     console.log("Value found:", value);
+// }
+
+// Clear All Values
+// sessionStorage.clear();
+
+
+// let value = sessionStorage.getItem(552);
+// console.log(typeof value);
+
+// Store Array in Session Storage
+// let names = ["Ali", "Enes", "Kübra", "Adem", "Ayşenur"];
+// sessionStorage.setItem("names", JSON.stringify(names));
+
+// Read Array from Session Storage
+// let value = JSON.parse(sessionStorage.getItem("names"));
+// value.forEach(function(name) {
+//     console.log(name);
+// });
+
+
+// Local Storage
+
+// Add Values
+// localStorage.setItem("motion1", "Push up");
+// localStorage.setItem("motion2", "Pull up");
+// localStorage.setItem("motion3", "Burpee");
+// localStorage.setItem("motion4", "Squat");
+
+
+// let value = localStorage.getItem("motion1");
+// console.log(value);
+
+
+// localStorage.removeItem("motion4");
+
+// Clear All
+// localStorage.clear();
+
+// Store Array in Local Storage
+// let motions = ["Push up", "Pull up", "Burpee", "Squat", "Chin Up"];
+// localStorage.setItem("motions", JSON.stringify(motions));
+
+// Read Array from Local Storage
+// let value = JSON.parse(localStorage.getItem("motions"));
+// value.forEach(function(motion) {
+//     console.log(motion);
+// });
