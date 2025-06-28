d7505cab69d3931a86c658672d15803b9c81b2ef
Gül Nihal Temel <diolasciarmi@gmail.com>
2025-06-29 00:15:45 +0300

    feat: demonstrate use of for...in and for...of loops in JavaScript

---
+++ for in&of loops/app.js
@@ -0,0 +1,30 @@
+//* FOR IN & FOR OF LOOPS 
+
+
+let names =["Valka","Eret","Gobber","Snotlout","Phlegma",]
+
+/*
+names.forEach(function(name){
+    console.log(name)
+})
+*/
+
+
+//names.forEach(name=>console.log(name))
+
+//! FOR IN(DEX) LOOP
+
+//? variableDeclaration - in - arrayName
+
+/*
+for(let name in names){                         // index    names[name]
+    console.log(name, names[name])
+}
+
+*/
+
+/*
+for( let name of names){                        // names[name]    index
+    console.log(name, names.indexOf(name))
+}
+    */
