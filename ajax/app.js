63b096220b589cbc6d53e8c1ab3d1598545c6f73
Gül Nihal Temel <diolasciarmi@gmail.com>
2025-07-10 02:50:00 +0300

    feat: introduce basic AJAX setup using XMLHttpRequest

---
+++ ajax/app.js
@@ -0,0 +1,16 @@
+//*AJAX
+
+//? AJAX = Asynchronous JavaScript And XML.
+
+//? AJAX is not a programming language.
+//? AJAX just uses a combination of: 
+//?     A browser built-in XMLHttpRequest object (to request data from a web server) 
+//?    JavaScript and HTML DOM (to display or use the data)
+
+//? AJAX allows web pages to be updated asynchronously by exchanging data with a web server behind the scenes. This means that it is possible to update parts of a web page, without reloading the whole page.
+
+
+//create a new XMLHttpRequest object
+const xhr = new XMLHttpRequest();
+console.log(xhr);
+
